a4e444db90ef9eaf65ff242794e2a48976842528
Michael Coker <mcoker@gmail.com>
2017-08-10 16:26:49 -0500

    mostly style updates, also check for ?channel

--- index.php
+++ index.php
@@ -26,29 +26,37 @@ font-family: Roboto, sans-serif;
             display: flex;
             flex-wrap: wrap;
         }
+        .curved {
+            border-radius: 5px;
+            overflow: hidden;
+        }
         .title {
             background: #000;
             padding: .5em 1em;
-            border-radius: 2px 2px 0 0;
             font-size: 2em;
         }
         .titleLink {
             color: #f0f0f0;
         }
-        .channelList {
+        .channel {
+            margin: 0 0 2em;
             background: #f0f0f0;
             border: 1px solid #000;
+        }
+        .channelList {
             display: flex;
             flex-wrap: wrap;
             padding: 1em 1em 0;
-            margin: 0 0 2em;
         }
         .vid {
             width: calc(25% - 1em);
             margin: 0 1em 1em 0;
-            text-align: center;
             display: flex;
-            flex-direction: column;
+            background: #ddd;
+            border: 1px solid #000;
+            padding: 0 0 .5em;
+            flex-wrap: wrap;
+            align-items: flex-start;
         }
         .vid:nth-child(4n) {
             margin-right: 0;
@@ -56,50 +64,53 @@ font-family: Roboto, sans-serif;
         .thumb {
             width: 100%;
             display: block;
+            margin: 0 0 .5em;
         }
         .vidTitle {
-            background: #ddd;
+            background: #ddd; */
             display: block;
-            padding: .5em;
-            flex-grow: 1;
             color: #000;
-            border: 1px solid #ccc;
+            border-left: 1px solid #666;
+            margin-left: 1em;
+            padding: 0 1em 0 .5em;
         }
     </style>
 </head>
 <body>
 <div class="page">
 <?
-$channel = $_GET['channel'];
-$channelArr = explode('|',$channel);
-foreach ($channelArr as $feed) {
-    $parts = explode(':',$feed);
-    $type = $parts[0];
-    $channelId = $parts[1];
-    $feed = implode(file('https://www.youtube.com/feeds/videos.xml?'.$type.'='.$channelId));
-    $xml = simplexml_load_string($feed);
-    $json = json_encode($xml);
-    $array = json_decode($json,TRUE);
-    $title = $array['title'];
-    $channelUrl = $array['author']['uri'];
-    ?>
-    <div class="channel">
-    <h2 class="title"><a class="titleLink" href="<?=$channelUrl;?>"><?=$title;?></a></h2>
-    <div class="channelList">
-    <?
-    foreach ($array['entry'] as $entry) {
-            $id = explode(':',$entry['id'])[2];
-            $url = 'https://www.youtube.com/watch?v='.$id;
-            $thumb = 'http://img.youtube.com/vi/'.$id.'/0.jpg';
-            $vidTitle = $entry['title'];
-            ?>
-                <a class="vid" href="<?=$url;?>"><img class="thumb" src="<?=$thumb;?>"><span class="vidTitle"><?=$vidTitle;?></span></a>
-            <?
+if (isset($_GET['channel'])) {
+    $channel = $_GET['channel'];
+    $channelArr = explode('|',$channel);
+    foreach ($channelArr as $feed) {
+        $parts = explode(':',$feed);
+        $type = $parts[0];
+        $channelId = $parts[1];
+        $feed = implode(file('https://www.youtube.com/feeds/videos.xml?'.$type.'='.$channelId));
+        $xml = simplexml_load_string($feed);
+        $json = json_encode($xml);
+        $array = json_decode($json,TRUE);
+        $title = $array['title'];
+        $channelUrl = $array['author']['uri'];
+        ?>
+        <div class="channel curved">
+        <h2 class="title"><a class="titleLink" href="<?=$channelUrl;?>"><?=$title;?></a></h2>
+        <div class="channelList">
+        <?
+        foreach ($array['entry'] as $entry) {
+                $id = explode(':',$entry['id'])[2];
+                $url = 'https://www.youtube.com/watch?v='.$id;
+                $thumb = 'http://img.youtube.com/vi/'.$id.'/0.jpg';
+                $vidTitle = $entry['title'];
+                ?>
+                    <a class="vid curved" href="<?=$url;?>"><img class="thumb" src="<?=$thumb;?>"><span class="vidTitle"><?=$vidTitle;?></span></a>
+                <?
+        }
+        ?>
+        </div>
+        </div>
+        <?
     }
-    ?>
-    </div>
-    </div>
-    <?
 }
 ?>
 </div>
